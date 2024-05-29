39300fc30a8953f5c5e26083f35675b465d99329
M Rohith <developer.mr101804@gmail.com>
2024-05-29 10:41:16 +0530

    first

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { spawn } = require("child_process");
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/",(req,res)=>{
+    res.json({"result":"Success!"})
+})
+
+app.post("/generate-plot", (req, res) => {
+    const inputData = req.body;
+
+    const pythonProcess = spawn("python", ["plot.py"]);
+
+    pythonProcess.stdin.write(JSON.stringify(inputData));
+    pythonProcess.stdin.end();
+
+    let dataChunks = [];
+    pythonProcess.stdout.on("data", (chunk) => {
+        dataChunks.push(chunk);
+    });
+
+    pythonProcess.stdout.on("end", () => {
+        const base64Image = Buffer.concat(dataChunks).toString();
+        res.json({ image: base64Image });
+    });
+
+    pythonProcess.stderr.on("data", (data) => {
+        console.error(`stderr: ${data}`);
+        res.json({ "error": data })
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
