44d46a9454c8d679b2bb2edb68f7a484274dd127
M Rohith <developer.mr101804@gmail.com>
2024-05-29 12:41:04 +0530

    third

--- server.py
+++ server.py
@@ -57,4 +57,6 @@ def generate_plot_endpoint():
     return jsonify({"image": base64_image})
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
